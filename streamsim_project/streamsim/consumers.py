e97b42162f4d7d24eefa6187deb0c1d43fa864bf
armanarstambekov <armanarstambekov@gmail.com>
2025-10-23 15:05:31 +0500

    test

---
+++ streamsim_project/streamsim/consumers.py
@@ -0,0 +1,14 @@
+from channels.generic.websocket import AsyncWebsocketConsumer
+import json
+
+class StreamConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
+        await self.send(text_data=json.dumps({"message": "Stream started"}))
+
+    async def disconnect(self, close_code):
+        pass
+
+    async def receive(self, text_data):
+        data = json.loads(text_data)
+        await self.send(text_data=json.dumps({"echo": data}))
